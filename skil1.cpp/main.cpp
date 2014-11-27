b12bcff820d53acf3a10c269b04261803a0d5a43
eythorsnaer <eythorsnaer@gmail.com>
2014-11-27 15:16:33 +0000

    bara prófa

---
+++ skil1.cpp/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  skil1.cpp
+//
+//  Created by Eyþór Snær Tryggvason on 27/11/14.
+//  Copyright (c) 2014 eythor. All rights reserved.
+//
+
+#include <iostream>
+#include "presentationLayer"
+
+int main(int argc, const char * argv[]) {
+    
+    presentationLayer::Programmer.start();
+    
+    return 0;
+}
